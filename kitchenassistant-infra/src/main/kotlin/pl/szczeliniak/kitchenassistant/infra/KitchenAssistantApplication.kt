44891c866cfdcd18bdea7c21d16d2f5773286271
marcin <marcin@szczeliniak.pl>
2022-03-12 20:15:33 +0100

    base modules

--- kitchenassistant-infra/src/main/kotlin/pl/szczeliniak/kitchenassistant/infra/KitchenAssistantApplication.kt
+++ kitchenassistant-infra/src/main/kotlin/pl/szczeliniak/kitchenassistant/infra/KitchenAssistantApplication.kt
@@ -1,11 +1,11 @@
-package pl.szczeliniak.kitchensassistant
+package pl.szczeliniak.kitchenassistant.infra
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class KitchensassistantApplication
+class KitchenAssistantApplication
 
 fun main(args: Array<String>) {
-	runApplication<KitchensassistantApplication>(*args)
+    runApplication<KitchenAssistantApplication>(*args)
 }
