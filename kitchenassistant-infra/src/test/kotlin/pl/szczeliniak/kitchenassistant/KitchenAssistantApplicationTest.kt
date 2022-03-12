ae1c7467ec321191c3d2b4c1170fcc776cd42e99
marcin <marcin@szczeliniak.pl>
2022-03-12 23:24:51 +0100

    swagger

---
+++ kitchenassistant-infra/src/test/kotlin/pl/szczeliniak/kitchenassistant/KitchenAssistantApplicationTest.kt
@@ -0,0 +1,11 @@
+package pl.szczeliniak.kitchenassistant
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KitchenAssistantApplicationTest
+
+fun main(args: Array<String>) {
+    runApplication<KitchenAssistantApplicationTest>(*args)
+}
