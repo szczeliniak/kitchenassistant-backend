732dd3c6da2fddcea80c76906d19813c14a0bd11
marcin <marcin@szczeliniak.pl>
2022-03-12 19:49:27 +0100

    init

---
+++ src/main/kotlin/pl/szczeliniak/kitchensassistant/KitchensassistantApplication.kt
@@ -0,0 +1,11 @@
+package pl.szczeliniak.kitchensassistant
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KitchensassistantApplication
+
+fun main(args: Array<String>) {
+	runApplication<KitchensassistantApplication>(*args)
+}
---
+++ src/test/kotlin/pl/szczeliniak/kitchensassistant/KitchensassistantApplicationTests.kt
@@ -0,0 +1,13 @@
+package pl.szczeliniak.kitchensassistant
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KitchensassistantApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
