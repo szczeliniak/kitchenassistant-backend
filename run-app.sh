cab1ccd6e10c38238dfc83c9bfdd05aaa7db60a6
Marcin Szczeliniak <marcin@szczeliniak.pl>
2022-06-28 21:51:09 +0200

    [Infra] docker scripts

--- run-app.sh
+++ run-app.sh
@@ -1,12 +1,10 @@
 #!/bin/sh
 
+#build image
 ./build-docker-image.sh
 
-#run db
-docker-compose -f docker-compose-db.yaml up -d
-
-#run ftp
-docker-compose -f docker-compose-ftp.yaml up -d
+#run infra
+docker-compose -f docker-compose-infra.yaml up -d
 
 #run app
 docker-compose -f docker-compose-app.yaml up -d
--- stop-app.sh
+++ stop-app.sh
@@ -3,8 +3,5 @@
 #stop app
 docker-compose -f docker-compose-app.yaml down
 
-#stop ftp
-docker-compose -f docker-compose-ftp.yaml up -d
-
-#stop db
-docker-compose -f docker-compose-db.yaml down
+#stop infra
+docker-compose -f docker-compose-infra.yaml down
