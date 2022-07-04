8e1c9d2108204dfe7a2392f6a4132a4e23d7009b
Marcin Szczeliniak <marcin@szczeliniak.pl>
2022-07-04 18:26:04 +0200

    [DayPlanDetails] fix

--- kitchenassistant-application/src/main/kotlin/pl/szczeliniak/kitchenassistant/dayplan/queries/dto/DayPlanDetailsDto.kt
+++ kitchenassistant-application/src/main/kotlin/pl/szczeliniak/kitchenassistant/dayplan/queries/dto/DayPlanDetailsDto.kt
@@ -3,7 +3,7 @@ package pl.szczeliniak.kitchenassistant.dayplan.queries.dto
 import java.time.LocalDate
 
 data class DayPlanDetailsDto(
-    private val id: Int,
-    private val date: LocalDate,
-    private val receipts: List<SimpleReceiptDto>
+    val id: Int,
+    val date: LocalDate,
+    val receipts: List<SimpleReceiptDto>
 )
