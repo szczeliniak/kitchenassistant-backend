d69c9a88783ef91b15dc5f7f82069ea11c479c40
Marcin Szczeliniak <marcin@szczeliniak.pl>
2022-09-10 17:43:26 +0200

    scheduler fix

--- kitchenassistant-infra/src/main/kotlin/pl/szczeliniak/kitchenassistant/dayplan/DayPlanScheduler.kt
+++ kitchenassistant-infra/src/main/kotlin/pl/szczeliniak/kitchenassistant/dayplan/DayPlanScheduler.kt
@@ -4,12 +4,14 @@ import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
 
 @Service
 class DayPlanScheduler(private val dayPlanFacade: DayPlanFacade) {
 
     private val logger: Logger = LoggerFactory.getLogger(DayPlanScheduler::class.java)
 
+    @Transactional
     @Scheduled(cron = "\${scheduling.dayplan.archiving}")
     fun terminateDayPlans() {
         logger.info("Day plan termination has been triggered!")
